25c8289d877182e97ed9bf9a01b1ff1cf046d75d
konradlewandowski <konrad.lewandowski92@gmail.com>
2023-05-12 18:10:36 +0200

    `add new state for handling spinner when loading`

--- src/contexts/error.context.tsx
+++ src/contexts/error.context.tsx
@@ -5,6 +5,8 @@ interface IValueProviderError {
   setModalShow: (data: boolean) => void;
   errorMessage: string;
   setErrorMessage: (data: string) => void;
+  loading: boolean;
+  setLoading: (data: boolean) => void;
 }
 
 const ErrorContext = createContext<IValueProviderError>({
@@ -12,17 +14,22 @@ const ErrorContext = createContext<IValueProviderError>({
   setModalShow: () => {},
   errorMessage: "",
   setErrorMessage: () => {},
+  loading: false,
+  setLoading: () => {},
 });
 
 export const ErrorProvider: TProvider = ({ children }) => {
   const [modalShow, setModalShow] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const value: IValueProviderError = {
     modalShow,
     setModalShow,
     errorMessage,
     setErrorMessage,
+    loading,
+    setLoading,
   };
 
   return (
