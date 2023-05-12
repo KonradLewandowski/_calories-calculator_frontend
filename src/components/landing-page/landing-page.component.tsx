ebd05f6c358d54836f76b8cae33f02d54c4c25dd
konradlewandowski <konrad.lewandowski92@gmail.com>
2023-05-12 18:09:05 +0200

    `create the landing page and the reset page`

---
+++ src/components/landing-page/landing-page.component.tsx
@@ -0,0 +1,7 @@
+import styles from "./landing.module.scss";
+
+const LandingPageComponent = () => {
+  return <div>LandingPageComponent</div>;
+};
+
+export default LandingPageComponent;
---
+++ src/components/reset-page/reset-page.component.tsx
@@ -0,0 +1,154 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import {
+  Button,
+  Form,
+  Container,
+  Row,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
+import {
+  fetchResetToken,
+  fetchResetPassword,
+} from "../../services/fetch-data.service";
+import { IEmailCredentials } from "../../interfaces/user.interface";
+import ErrorContext from "../../contexts/error.context";
+
+import styles from "./reset-page.module.scss";
+
+enum ISubmitType {
+  Password = "password",
+  Token = "token",
+}
+const ResetPageComponent = () => {
+  const { setModalShow, setErrorMessage, setLoading } =
+    useContext(ErrorContext);
+  const [submitType, setSubmitType] = useState<ISubmitType>(
+    ISubmitType.Password
+  );
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<IEmailCredentials>();
+
+  const handleResetTokenSubmit = async (input: IEmailCredentials) => {
+    try {
+      const response = await fetchResetToken(input);
+
+      if (response.hasOwnProperty("errorMessage")) {
+        setModalShow(true);
+        setErrorMessage(response.errorMessage);
+        return;
+      }
+
+      setModalShow(true);
+      setErrorMessage("The token has been sent. Check your email.");
+
+      navigate("/login");
+    } catch (error) {
+      setModalShow(true);
+      setErrorMessage("An error occurred while resending the token");
+      console.error("LoginFormComponent Error: ", error);
+    }
+    setLoading(false);
+  };
+
+  const handleResetPasswordSubmit = async (input: IEmailCredentials) => {
+    try {
+      const response = await fetchResetPassword(input);
+
+      if (response.hasOwnProperty("errorMessage")) {
+        setModalShow(true);
+        setErrorMessage(response.errorMessage);
+        return;
+      }
+
+      setModalShow(true);
+      setErrorMessage(
+        "The reset password token has been sent. Check your email."
+      );
+
+      navigate("/login");
+    } catch (error) {
+      setModalShow(true);
+      setErrorMessage("An error occurred while resetting password.");
+      console.error("LoginFormComponent Error: ", error);
+    }
+    setLoading(false);
+  };
+
+  const handleFormSubmit = (input: IEmailCredentials) => {
+    setLoading(true);
+    submitType === ISubmitType.Token
+      ? handleResetTokenSubmit(input)
+      : handleResetPasswordSubmit(input);
+  };
+
+  return (
+    <Container fluid="sm">
+      <Row
+        className={`d-flex align-items-center justify-content-center ${styles.klRow}`}
+      >
+        <Form
+          id="resetForm"
+          className={`${styles.klRow__resendForm} shadow-lg rounded-4 p-4`}
+          onSubmit={handleSubmit(handleFormSubmit)}
+        >
+          <Form.Group className="mb-3" controlId="formBasicLogin">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter email address"
+              {...register("email", { required: "Required" })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.email?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <ToggleButtonGroup
+            className="mb-3"
+            type="radio"
+            name="submitType"
+            value={submitType}
+            onChange={(value) => setSubmitType(value)}
+          >
+            <ToggleButton
+              id="password-toggle"
+              value={ISubmitType.Password}
+              variant="outline-secondary"
+            >
+              Reset password
+            </ToggleButton>
+            <ToggleButton
+              id="token-toggle"
+              value={ISubmitType.Token}
+              variant="outline-secondary"
+            >
+              Reset token
+            </ToggleButton>
+          </ToggleButtonGroup>
+
+          <div className="text-center">
+            <Button
+              className="mb-3 "
+              variant="outline-secondary"
+              type="submit"
+              form="resetForm"
+              disabled={isSubmitting}
+            >
+              Send
+            </Button>
+          </div>
+        </Form>
+      </Row>
+    </Container>
+  );
+};
+
+export default ResetPageComponent;
